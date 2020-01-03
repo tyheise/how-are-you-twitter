2b298dc979a81f090ccc5d27c7f681af1b4163ee
Tyler Heise <tyler.heise@gmail.com>
2020-01-02 20:25:04 -0700

    Added model for storing the token

--- howareyoutwitter/api/models.py
+++ howareyoutwitter/api/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
 # Create your models here.
+class Token(models.Model):
+    token_type = models.CharField(max_length=300)
+    access_token = models.CharField(max_length=300)
--- howareyoutwitter/api/views.py
+++ howareyoutwitter/api/views.py
@@ -5,11 +5,14 @@ import os
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from api.models import Token
 
 class AuthenticateView(APIView):
 
     def get(self, request):
         credentials = twitter.Api().GetAppOnlyAuthToken(os.environ['CONSUMER_KEY'], os.environ['CONSUMER_SECRET'])
+        token = Token(token_type=credentials['token_type'], access_token=credentials['access_token'])
+        token.save()
         return Response(credentials)
 
         
