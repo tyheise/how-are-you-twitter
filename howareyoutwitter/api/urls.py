52059567a6bacd11b8c42718c93c93e39e944a7d
tyheise <tyler.heise@gmail.com>
2021-01-02 00:49:17 -0800

    Exposing this backend logic in views doesn't make sense. We will just schedule a task to run this code instead of pinging a URL endpoint.

--- howareyoutwitter/api/urls.py
+++ howareyoutwitter/api/urls.py
@@ -2,7 +2,4 @@ from django.urls import path
 
 from . import views
 
-urlpatterns = [
-    path('auth', views.AuthenticateView.as_view(), name='authenticate-view'),
-    path('search', views.SearchTweetsView.as_view(), name='search-tweets-view')
-]
+urlpatterns = []
